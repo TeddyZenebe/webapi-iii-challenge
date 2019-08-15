395953bbcac5eee9be4195ad3453ed4048eb7819
Your Name <tfikrie@yahoo.com>
2019-08-15 19:58:37 -0400

    prepared for depoloyment

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // code away!
-
+require('dotenv').config()
 const server = require('./server')
+const port = process.env.PORT || 4000
 
-server.listen(4000, () => {
-  console.log('\n* Server Running on http://localhost:4000 *\n');
+server.listen(port, () => {
+  console.log(`\n* Server Running on PORT http://localhost:${port} *\n`);
 })
